20a21ebb6ba5ba1581c6da122ce3a2d7f68f74b0
Isaacn123 <nsambai72@gmail.com>
2025-07-17 06:47:33 +0300

    adding headersettings app

--- webadmin/site_settings/views.py
+++ webadmin/site_settings/views.py
@@ -1,19 +1,6 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import HeaderSettings
 from .serializers import HeaderSettingsSerializer
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import HeaderSettingsViewSet
-
-router = DefaultRouter()
-router.register(r'header-settings', HeaderSettingsViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
-
-# Create your views here.
 
 class HeaderSettingsViewSet(viewsets.ModelViewSet):
     queryset = HeaderSettings.objects.all()
