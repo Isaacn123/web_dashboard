87992b8aca5111d91997fa30d3a6505d82c8185f
Isaacn123 <nsambai72@gmail.com>
2025-07-17 03:35:17 +0300

    changing localhost or 127.0.0.1 to 45.56.120.65

--- webadmin/mysite/settings.py
+++ webadmin/mysite/settings.py
@@ -187,7 +187,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://45.56.120.65:3000",
-    "http://45.56.120.65:8000/",
+    "http://45.56.120.65:8000",
+    "http://45.56.120.65:8080"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
