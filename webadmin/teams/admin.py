33e48b9fcf66bdf14a946c96ef589ad929eb6689
Isaacn123 <nsambai72@gmail.com>
2025-07-18 21:30:20 +0300

    adding backend service

--- webadmin/teams/admin.py
+++ webadmin/teams/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import TeamMember
 # Register your models here.
-admin.site.admin.site.register(TeamMember)
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ('name' ,'role','photo','email','phone','twitter','linkedin','order ','active')
+    search_fields = ('name', 'role')
+    list_filter = ('role', 'active')
+
+    
