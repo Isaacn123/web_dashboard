b1e8660eeded048091c08e6ff9df829e88816d1a
Isaacn123 <nsambai72@gmail.com>
2025-07-19 08:33:36 +0300

    adding teams table

--- webadmin/teams/urls.py
+++ webadmin/teams/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TeamMemberViewSet, test_team_members
+from .views import TeamMemberViewSet
 
 
 router = DefaultRouter()
 router.register(r'team-members', TeamMemberViewSet)
 urlpatterns = [
     path('', include(router.urls)),
-    path('test-team-members/', test_team_members, name='test_team_members'),
 ]
 
