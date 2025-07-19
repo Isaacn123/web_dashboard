9b115e72c71b448973358cee751dfa86ac781de3
Isaacn123 <nsambai72@gmail.com>
2025-07-19 16:06:13 +0300

    adding teams table

--- frontend/src/app/create-article/CreateArticleClient.tsx
+++
@@ -1,5 +0,0 @@
-// "use client";
-// import { useState, useEffect } from 'react';
-// import { useRouter } from 'next/navigation';
-// import Link from 'next/link';
-// import { useAuth } from '../contexts/AuthContext';
