30ffc775740b641b5df97f83292227fc26a4bfb3
Isaacn123 <nsambai72@gmail.com>
2025-07-19 07:41:53 +0300

    adding teams table

--- webadmin/teams/models.py
+++ webadmin/teams/models.py
@@ -11,11 +11,13 @@ class TeamMember(models.Model):
     facebook = models.URLField(blank=True, null=True)
     twitter = models.URLField(blank=True, null=True)
     linkedin = models.URLField(blank=True, null=True)
-    order = models.PositiveIntegerField(default=0)  # For sorting
-    active = models.BooleanField(default=True)
+    order = models.PositiveIntegerField(default=0)  # type: ignore # For sorting
+    active = models.BooleanField(default=True)  # type: ignore
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
     
     class Meta:
-        ordering = ['order']
+        ordering = ['created_at']
