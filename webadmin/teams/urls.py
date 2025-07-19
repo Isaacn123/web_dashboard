8d5a860088b4b14cbce5aea88ed8fe865921e01c
Isaacn123 <nsambai72@gmail.com>
2025-07-19 08:16:13 +0300

    adding teams table

--- webadmin/teams/urls.py
+++ webadmin/teams/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TeamMemberViewSet
+from .views import TeamMemberViewSet, test_team_members
 
 
 router = DefaultRouter()
 router.register(r'team-members', TeamMemberViewSet)
 urlpatterns = [
     path('', include(router.urls)),
+    path('test-team-members/', test_team_members, name='test_team_members'),
 ]
 
--- webadmin/teams/views.py
+++ webadmin/teams/views.py
@@ -1,6 +1,8 @@
 from .models import TeamMember
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 from .serializers import TeamMemberSerializer
 
 
@@ -9,3 +11,18 @@ class TeamMemberViewSet(viewsets.ModelViewSet):
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
     permission_classes = [AllowAny]  # Temporarily allow all access for testing
+
+# Test view to debug the issue
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def test_team_members(request):
+    if request.method == 'GET':
+        members = TeamMember.objects.all()
+        serializer = TeamMemberSerializer(members, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TeamMemberSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
